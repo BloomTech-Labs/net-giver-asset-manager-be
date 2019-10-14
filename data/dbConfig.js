c6294f242b6cfa691ff7efd4787c417692798d39
ebisLab <ebaiden@techie.com>
2019-10-14 14:01:01 -0400

    env file retracted

--- data/dbConfig.js
+++
@@ -1,9 +0,0 @@
-const knex = require('knex')
-const secrets = require('../config/secrets')
-const environment = secrets.environment || 'development'
-
-const dbConfig = require('../knexfile.js')[environment]
-
-
-
-module.exports = knex(dbConfig)
--- index.js
+++
@@ -1,7 +0,0 @@
-const server = require('./server/server')
-
-const PORT = process.env.PORT || 8000
-
-server.listen(PORT, () => {
-    console.log("Server is currently running on port", PORT)
-})
