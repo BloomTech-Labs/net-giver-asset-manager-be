9e87e412647f7c9920481afbfca2704c932b66cd
Kevin Lopez <kevin_lopez@live.com>
2019-10-14 17:21:55 -0500

    Revert "Email/password authentication"

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
+require('dotenv').config()
+
 const express = require('express')
 const server = express()
 
 const cors = require('cors')
 const helmet = require('helmet')
 
-const authRouter = require('../api/auth/auth-router.js');
-
 
 server.use(express.json())
 server.use(cors())
 server.use(helmet()) 
 
-server.use('/api/auth', authRouter);
 
 server.get('/', (req, res) => {
     res.status(200).send("We are live!")
