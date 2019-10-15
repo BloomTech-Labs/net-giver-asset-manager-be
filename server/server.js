92ddc09295884646d342eefff368d1391ed52d1f
TheHaunted <kevin_lopez@live.com>
2019-10-15 15:14:15 -0500

    adds authentication middleware globally in history-router

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const server = express()
 const cors = require('cors')
 const helmet = require('helmet')
 
+const authenticate = require('../api/auth/auth-middleware')
+
 const authRouter = require('../api/auth/auth-router.js');
 const assetsRouter = require('../api/assets/assets-router.js');
 const historyRouter = require('../api/history/history-router.js');
@@ -15,7 +17,7 @@ server.use(helmet())
 
 server.use('/api/auth', authRouter);
 server.use('/api/assets', assetsRouter);
-server.use('/api/history', historyRouter);
+server.use('/api/history', authenticate, historyRouter);
 
 server.get('/', (req, res) => {
     res.status(200).send("We are live!")
