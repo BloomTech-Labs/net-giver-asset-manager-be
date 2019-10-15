ea9664449fcdca16f54ce7e2297c5c7bb84c26dc
TheHaunted <kevin_lopez@live.com>
2019-10-15 13:52:52 -0500

    creates history table and seed

--- data/migrations/20191014152659_users.js
+++ data/migrations/20191014152659_users.js
@@ -2,13 +2,21 @@
 exports.up = function(knex) {
   return knex.schema
     .createTable('users', tbl => {
-        tbl.increments('id');
-        tbl.string('email').notNullable().unique();
-        tbl.string('password').notNullable();
+      tbl.increments('id');
+      tbl.string('email').notNullable().unique();
+      tbl.string('password').notNullable();
+    })
+    .createTable('history', tbl => {
+      tbl.increments('id');
+      tbl.string('asset_id').notNullable();
+      tbl.string('user_id').notNullable();
+      tbl.string('time_in').notNullable();
+      tbl.string('time_out').notNullable();
     })
 };
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists('users');
+    .dropTableIfExists('users')
+    .dropTableIfExists('history');
 };
