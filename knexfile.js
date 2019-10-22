365896c6e14a5429eb965bea326cfcdde32f1802
johnschneider1 <johnlawschneider@gmail.com>
2019-10-22 13:16:32 -0500

    changed pragma

--- knexfile.js
+++ knexfile.js
@@ -7,11 +7,11 @@ module.exports = {
     connection: {
       filename: "./data/netGiver.sqlite3"
     },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
-    },
+    // pool: {
+    //   afterCreate: (conn, done) => {
+    //     conn.run("PRAGMA foreign_keys = ON", done);
+    //   }
+    // },
     useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations"
