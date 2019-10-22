2e406a86eff9bbfc7f9a34f301f235e89761911a
johnschneider1 <johnlawschneider@gmail.com>
2019-10-22 14:18:54 -0500

    fixed seed asset_id

--- data/seeds/002-history.js
+++ data/seeds/002-history.js
@@ -7,17 +7,17 @@ exports.seed = function(knex) {
       return knex("history").insert([
         {
           id: 1,
-          asset_id: 2,
-          user_id: 3
+          asset_id: 1,
+          user_id: 1
         },
         {
           id: 2,
-          asset_id: 2,
+          asset_id: 1,
           user_id: 2
         },
         {
           id: 3,
-          asset_id: 3,
+          asset_id: 1,
           user_id: 3
         }
       ]);
