f3addf51bc7526c208c968e80fcacd88978febbf
johnschneider1 <johnlawschneider@gmail.com>
2019-10-22 13:03:26 -0500

    fixing seeding issue with Kevin Lopez changed tables and migrations

--- data/seeds/002-history.js
+++ data/seeds/002-history.js
@@ -1,30 +1,24 @@
-
-exports.seed = function (knex) {
+exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('history').del()
-    .then(function () {
+  return knex("history")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('history').insert([
+      return knex("history").insert([
         {
-          id: '1',
-          asset_id: '1',
-          user_id: '1',
-          time_in: '12:00',
-          time_out: '12:01'
+          id: "1",
+          asset_id: "1",
+          user_id: "1"
         },
         {
-          id: '2',
-          asset_id: '2',
-          user_id: '2',
-          time_in: '4:00',
-          time_out: '4:01'
+          id: "2",
+          asset_id: "2",
+          user_id: "2"
         },
         {
-          id: '3',
-          asset_id: '3',
-          user_id: '3',
-          time_in: '6:00',
-          time_out: '6:01'
+          id: "3",
+          asset_id: "3",
+          user_id: "3"
         }
       ]);
     });
