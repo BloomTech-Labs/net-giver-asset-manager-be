a8d05fcd40bb0f1e19c3a0260ec3332d914079b4
johnschneider1 <johnlawschneider@gmail.com>
2019-11-01 14:31:48 -0500

    fixed error with profile.js

--- api/aws/profile.js
+++ api/aws/profile.js
@@ -4,6 +4,7 @@ const multerS3 = require("multer-s3");
 const multer = require("multer");
 const path = require("path");
 const url = require("url");
+const env = require("./s3.env");
 
 const router = express.Router();
 
