4aa22b8ab7cf472161a5f9669ee288cbe2403f03
Vicky <vyue013@gmail.com>
2019-11-06 12:36:31 -0800

    added comment for phone number addition to user schema

--- server/model/user_model.js
+++ server/model/user_model.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose'),
 const UserSchema = new Schema({
     username: {type: String, required: true, unique: true},
     email: {type: String, required: true},
-    
+    // phone_number
     authyId: String,
     hashed_password: String
 });
